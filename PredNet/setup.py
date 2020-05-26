526c1f7cbbf463016f05156f6a3ac531dfe99c36
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-26 09:48:10 +0200

    [PredNet] Implement setup.py/requirements.txt and most basic files
    - README: Adding a README for users
    - setup.py: Adding a python setup file for convencience
    - requirements.txt: Adding a pip requirements for convencience
    - main.py: Adding the main file
    - train.py: Adding the training file
    - test.py: Adding the testing file
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

---
+++ PredNet/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 26.05.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: Convienience file to install all necessary libraries etc.
+"""
+
+from setuptools import setup
+
+NAME = 'PredNet PyTorch'
+DESCRIPTION = 'Re-implementation of PredNet using PyTorch'
+AUTHOR = 'Sören S. Dittrich'
+AUTHOR_EMAIL = 'dittr002@uni-hildesheim.de'
+REQUIRES_PYTHON = '>=3.8.0'
+VERSION = '0.0.1'
+
+REQUIRED = [
+    'numpy', 'torch'
+]
+
+with open('README') as f:
+    readme = f.read()
+    
+setup(
+      name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=readme,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      python_requires=REQUIRES_PYTHON,
+      install_requires=REQUIRED
+)
---
+++ PredNet/train.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 26.05.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: Train-file
+""" 
