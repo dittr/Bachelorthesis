bb4a4e7f907265dfdf985a48f802f58c981afa77
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-26 11:36:47 +0200

    [PredNet] Adding input layer
    - input.py Adding the input layer (Used if l>0)
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

---
+++ PredNet/model/modules/input.py
@@ -0,0 +1,32 @@
+ #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 26.05.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: Input layer
+"""
+
+import torch.nn as nn
+import torch.nn.functional as f
+
+class InputLayer(nn.Module):
+    """
+    """
+    def __init__(self, channel_in, channel_out, kernel_size, padding):
+        """
+        """
+        super(InputLayer, self).__init__()
+        
+        self.conv = nn.Conv2d(in_channels=channel_in,
+                              out_channels=channel_out,
+                              kernel_size=kernel_size[0],
+                              padding=padding)
+        self.pool = nn.MaxPool2d(kernel_size=kernel_size[1])
+
+    def forward(self, x):
+        """
+        """
+        return self.pool(f.relu(self.conv(x)))
+        
