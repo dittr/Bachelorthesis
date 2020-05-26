40321eee433ab9ca4fb5d6f83681f91a954f3999
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-26 11:27:34 +0200

    [PredNet] Adding error module
    - error.py Adding the error module
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

---
+++ PredNet/model/modules/error.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 26.05.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: Error layer
+"""  
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+class ErrorLayer(nn.Module):
+    """
+    """
+    def __init__(self):
+        """
+        """
+        super(ErrorLayer, self).__init__()
+        
+    def forward(x, y):
+        """
+        """
+        return torch.cat((f.relu(x - y), f.relu(y - x)), 0)
