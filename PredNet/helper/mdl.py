57102ca8a5acb749e21cb6b451ebe126f72bc764
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-27 11:33:45 +0200

    [PredNet] Adding log option to the file
    If debug is True, the user is able to have log output as well.
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

--- PredNet/helper/mdl.py
+++ PredNet/helper/mdl.py
@@ -12,6 +12,7 @@ from dataclasses import dataclass
 from collections import OrderedDict
 import torch
 
+
 @dataclass
 class ModelParameter:
     epoch: int
@@ -25,53 +26,72 @@ class ModelSaver():
     """
     Class to save a model to a certain path
     """
-    def __init__(self, dataset, path='mdl/', mdl='prednet', debug=False):
+    def __init__(self, dataset, path='mdl/', mdl='prednet',
+                 debug=False, logger=None):
         """
         """
         self.dataset = dataset
         self.path = path 
         self.mdl = mdl
         self.debug
+        self.logger = logger
 
     def save(self, param):
         """
         """
+        file = self.path + self.mdl + '_' + self.dataset + '.pth'
         torch.save({
                 'epoch': param.epoch,
                 'iteration': param.iteration,
                 'model_state': param.mdl_state,
                 'optimizer_state': param.optim_state,
                 'loss': param.loss,
-                }, self.path + self.mdl + '_' + self.dataset + '.pth')
-        
+                }, file)
+
         if self.debug:
-            print('[ModelSaver] Model saved.')
+            deb = '[ModelSaver] Epoch: {}, Iteration: {}, State: {}, Optim: {},\
+                  Loss: {}, File: {}'.format(param.epoch, param.iteration,
+                                             param.mdl_state, param.optim_state,
+                                             param.loss, file)
+            print(deb)
+            if type(self.logger) != None:
+                self.logger.write(deb)
 
 
 class ModelLoader():
     """
     Class to load a model from a certain path
     """
-    def __init__(self, dataset, path='mdl/', mdl='prednet', debug=False):
+    def __init__(self, dataset, path='mdl/', mdl='prednet',
+                 debug=False, logger=None):
         """
         """
         self.dataset = dataset
         self.path = path
         self.mdl = mdl
         self.debug = debug
+        self.logger = logger
 
     def load(self):
         """
         """
         param = ModelParameter
-        checkp = torch.load(self.path + self.mdl + '_' +
-                            self.dataset + '.pth')
-        
+        file = self.path + self.mdl + '_' + self.dataset + '.pth'
+        checkp = torch.load(file)
+
         param.epoch = checkp['epoch']
         param.iteration = checkp['iteration']
         param.mdl_state = checkp['model_state']
         param.optim_state = checkp['optimizer_state']
         param.loss = checkp['loss']
         
+        if self.debug:
+            deb = '[ModelSaver] Epoch: {}, Iteration: {}, State: {}, Optim: {},\
+                  Loss: {}, File: {}'.format(param.epoch, param.iteration,
+                                             param.mdl_state, param.optim_state,
+                                             param.loss, file)
+            print(deb)
+            if type(self.logger) != None:
+                self.logger.write(deb)
+
         return param
-        
