be1ff8c0215e1062565c4c9b0a5aaa900f0a0fce
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-28 14:06:16 +0200

    [PredNet] Adding testing method
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

--- PredNet/test.py
+++ PredNet/test.py
@@ -2,8 +2,42 @@
 # -*- coding: utf-8 -*-
 
 """
-@date: 26.05.2020
+@date: 28.05.2020
 @author: Sören S. Dittrich
-@version: 0.0.1
-@description: Test-file
+@version: 0.0.2
+@description: Test file
 """ 
+
+import torch
+
+from helper.loss import loss as Loss
+
+def test(model, lossp, dataloader, logger, device):
+    # set model in evaluation mode
+    model.eval()
+
+    it = 0
+
+    # run through all batches in the dataloader
+    for batch_id, (data, target) in enumerate(dataloader):
+        # get sequence and target (This is only for gray-scaled images.)
+        x = data.float().to(device)[:,:,None,:,:].permute(1,0,2,3,4)
+        y = target.float().to(device)[:,:,None,:,:].permute(1,0,2,3,4)
+
+        # forward pass
+        output = model(x)
+
+        # compute loss
+        loss = Loss(output[-1], y, lossp)
+
+        it += 1
+
+        # log scalar values
+        logger.plot_loss(loss.item(), it)
+
+        # log images
+        x = torch.cat((x[1:], y), dim=0).permute(1,0,2,3,4)[0]
+        output = output.permute(1,0,2,3,4)[0]
+        
+        logger.plot_images('ground_truth', x)
+        logger.plot_images('predicted', output)
