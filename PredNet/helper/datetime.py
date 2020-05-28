0333ec27f5861511d4d2ef5ba0480d160dd7a58d
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-28 09:54:19 +0200

    [PredNet] Adding datetime helper
    This should help to get date and time easy.
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

---
+++ PredNet/helper/datetime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 28.05.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: Datetime module (Not mandatory)
+""" 
+
+import datetime
+
+def get_today():
+    """
+    Simple wrapper to get todays value
+    """
+    return datetime.date.today()
+
+def get_time():
+    """
+    Simple wrapper to get the time in format: hour_minute
+    """
+    return str(datetime.datetime.now().hour) + \
+           '_' + str(datetime.datetime.now().minute)
