634d1908fc10f74162c965c213a13451c15d0765
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-05-30 15:16:51 +0200

    [PredNet] Adding MovingMNIST dataset
    This is only a wrapper for two created hkl-files.
    The creation of training and testing is done via:
    https://gist.github.com/praateekmahajan/b42ef0d295f528c986e2b3a0b31ec1fe
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

---
+++ PredNet/dataset/MovingMNIST.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 30.05.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: MovingMNIST dataset for PredNet
+"""
+
+import os
+import torch
+import torch.utils.data as data
+import hickle as hkl
+
+
+class MovingMNIST(data.Dataset):
+    """
+    """
+    
+    training_file = 'moving_mnist_train.hkl'
+    test_file = 'moving_mnist_test.hkl'
+    
+    def __init__(self, root, testing=False):
+        """
+        """
+        self.root = root
+        self.testing = False        
+        self.train_data = torch.from_numpy(hkl.load(os.path.join(self.root,
+                                                                 self.training_file)))
+        self.test_data = torch.from_numpy(hkl.load(os.path.join(self.root,
+                                                                self.test_file)))
+        
+        
+    def __getitem__(self, index):
+        """
+        """
+        if not self.testing:
+            return self.train_data[index]
+        else:
+            return self.test_data[index]
+
+
+    def __len__(self):
+        """
+        """
+        if self.testing:
+            return len(self.train_data)
+        else:
+            return len(self.test_data)
