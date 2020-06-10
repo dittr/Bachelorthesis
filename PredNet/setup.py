2153243c554f877b038a9f3969d2f9181aa8bef8
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-06-10 18:37:14 +0200

    [PredNet] Add ssim library to requirements and setup
    SSIM (structural similarity index measure) is not part of PyTorch, therefore use
    a small addon available in pip.
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

--- PredNet/setup.py
+++ PredNet/setup.py
@@ -18,7 +18,7 @@ REQUIRES_PYTHON = '>=3.8.0'
 VERSION = '0.0.1'
 
 REQUIRED = [
-    'numpy', 'matplotlib', 'torch', 'tensorboard', 'PyYAML', 'hickle', 'torchviz', 'graphviz', 'opencv-python'
+    'numpy', 'matplotlib', 'torch', 'tensorboard', 'PyYAML', 'hickle', 'torchviz', 'graphviz', 'opencv-python', 'pytorch-msssim'
 ]
 
 with open('README') as f:
