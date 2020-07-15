cf98e80fa1cad55eb3822fa8dd65f88f9b3f8666
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-07-15 15:09:10 +0200

    [ConvLSTM] Adding ConvLSTM as baseline
    Appending the code for ConvLSTM baseline
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

---
+++ PredNet/model/autoenc.py
@@ -0,0 +1,77 @@
+ #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@date: 15.07.2020
+@author: Sören S. Dittrich
+@version: 0.0.1
+@description: ConvLSTM/PredRNN Autoencoder
+"""
+
+import torch.nn as nn
+
+from model.modules.conv_lstm import CONV_LSTM
+from model.modules.predrnn import PredRNN
+
+
+class AutoENC(nn.Module):
+    """
+    """
+    def __init__(self, depth, channel, kernel,
+                 padding, predrnn=False, gpu=False):
+        """
+        """
+        super(AutoENC, self).__init__()
+        self.name = 'convlstm'
+        
+        self.depth = depth
+        self.predrnn = predrnn
+        
+        self.encoder = nn.ModuleList()
+        self.decoder = nn.ModuleList()
+        
+        for i in range(self.depth):
+            if self.predrnn:
+                self.encoder.append(PredRNN(channel_in=channel[i],
+                                            channel_hidden=channel[i+1],
+                                            kernel=kernel,
+                                            padding=padding,
+                                            gpu=gpu))
+                self.decoder.append(PredRNN(channel_in=channel[-1 - i],
+                                            channel_hidden=channel[-1 - (i+1)],
+                                            kernel=kernel,
+                                            padding=padding,
+                                            gpu=gpu))
+            else:
+                self.encoder.append(CONV_LSTM(depth=1, channel_in=channel[i],
+                                              channel_hidden=channel[i+1],
+                                              kernel_size=kernel,
+                                              gpu=gpu))
+                self.decoder.append(CONV_LSTM(depth=1, channel_in=channel[-1 - i],
+                                              channel_hidden=channel[-1 - (i+1)],
+                                              kernel_size=kernel,
+                                              gpu=gpu))
+
+
+    def forward(self, x):
+        """
+        """    
+        if self.predrnn:
+            # 1. Encoder
+            for i in range(self.depth):
+                x, (h, c, m) = self.encoder[i](x)
+
+            # 2. Decoder
+            for i in range(self.depth):
+                x, (h, c, m) = self.decoder[i](x)
+        
+        else:
+            # 1. Encoder
+            for i in range(self.depth):
+                x, (h, c) = self.encoder[i](x)
+
+            # 2. Decoder
+            for i in range(self.depth):
+                x, (h, c) = self.decoder[i](x)
+
+        return x
