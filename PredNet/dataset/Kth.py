8ee24f4e81042700d4199dc215a4327e90c00f4a
Sören S. Dittrich <dittr002@uni-hildesheim.de>
2020-09-01 08:43:53 +0200

    [PredNet] Add early_stopping from local machine
    This implementation was used for the second experiments, but not uploaded.
    The early_stopping is not my code!
    
    Signed-off-by: Sören S. Dittrich <dittr002@uni-hildesheim.de>

--- PredNet/dataset/Kth.py
+++ PredNet/dataset/Kth.py
@@ -20,26 +20,36 @@ class Kth(data.Dataset):
 
     test_file = 'kth_test.hkl'
     training_file = 'kth_train.hkl'
+    validation_file = 'kth_dev.hkl'
 
-    def __init__(self, root, seq_len, testing=False):
+    def __init__(self, root, seq_len, testing=False, validation=False):
         """
         """
         self.root = root
         self.seq_len = seq_len
         self.testing = testing
+        self.validation = validation
         
-        if not self.testing:
-            self.train_data = hkl.load(os.path.join(self.root,
-                                                    self.training_file))
-            self.train_data = self._create_tensor(self.train_data)
+        if self.validation:
+            self.validation_data = hkl.load(os.path.join(self.root,
+                                                         self.validation_file))
+            self.validation_data = self._create_tensor(self.validation_data)
         else:
-            self.test_data = hkl.load(os.path.join(self.root, self.test_file))
-            self.test_data = self._create_tensor(self.test_data)
+            if not self.testing:
+                self.train_data = hkl.load(os.path.join(self.root,
+                                                        self.training_file))
+                self.train_data = self._create_tensor(self.train_data)
+            else:
+                self.test_data = hkl.load(os.path.join(self.root, self.test_file))
+                self.test_data = self._create_tensor(self.test_data)
 
 
     def __getitem__(self, index):
         """
         """
+        if self.validation:
+            return self.validation_data[index][:,None,:,:]
+        
         if not self.testing:
             return self.train_data[index][:,None,:,:]
         else:
@@ -49,6 +59,9 @@ class Kth(data.Dataset):
     def __len__(self):
         """
         """
+        if self.validation:
+            return self.validation_data.size(0)
+        
         if not self.testing:
             return self.train_data.size(0)
         else:
